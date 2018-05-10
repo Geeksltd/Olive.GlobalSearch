17b85178e141550ef7cc16f3d62d7f351315352b
Mohsen <mohsens2@outlook.com>
2018-05-10 08:35:32 +0430

    deleted unused IUser and User implementation

--- Olive.GlobalSearch/Olive.GlobalSearch.UI/Search.cs
+++ Olive.GlobalSearch/Olive.GlobalSearch.UI/Search.cs
@@ -11,12 +11,9 @@ namespace Olive.GlobalSearch.UI
         public static async Task<IEnumerable<SearchResult>> AutoComplete(string keywords)
         {
             var results = new List<SearchResult>();
-            var urls = Config.Get("Me:Name");
-            System.Diagnostics.Debug.WriteLine(urls);
-            results.AddRange(await new ApiClient($"http://localhost:9015/api/search?term={keywords}").AsHttpUser().Get<SearchResult[]>());
-            //TODO: Get from settings
-            //foreach (var url in urls)
-            // results.AddRange(await new ApiClient($"{url.Value}/api/search?term={keywords}").AsHttpUser().Get<SearchResult[]>());
+            var urls = Config.SettingsUnder("Olive.GlobalSearch:Sources");
+            foreach (var url in urls)
+                results.AddRange(await new ApiClient($"{url.Value}api/search?term={keywords}").AsHttpUser().Get<SearchResult[]>());
 
             return results;
         }
