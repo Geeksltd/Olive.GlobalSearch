b6f2f6d7fb3edf0253cff533e2e9498a2e47cf8d
Mohsen <mohsens2@outlook.com>
2018-05-10 20:33:54 +0430

    no message

---
+++ Olive.GlobalSearch/Olive.GlobalSearch.Source/Extensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace Olive.GlobalSearch
+{
+    public static class Extensions
+    {
+        public static IApplicationBuilder UseGlobalSearch<T>(this IApplicationBuilder @this) where T : SearchSource, new()
+        {
+            @this.Map("/api/search", app => { app.Run(async context => await SearchApiMiddleware.Search<T>(context)); });
+            return @this;
+        }
+    }
+}
