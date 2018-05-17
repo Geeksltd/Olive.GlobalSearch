2078673660be0fddc940105acd3d7eed1cda772c
Mohsen <mohsens2@outlook.com>
2018-05-17 17:24:54 +0430

    removed UI project

--- Olive.GlobalSearch/Olive.GlobalSearch.UI/JsonItem.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Olive.GlobalSearch
-{
-    public class JsonItem
-    {
-        public string Display { get; set; }
-        public string Text { get; set; }
-        public object Value { get; set; }
-    }
-}
