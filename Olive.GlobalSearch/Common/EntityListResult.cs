d033c81d337c1d642d15ee9d04e7e2e12ea19308
Soroush Asadzadeh <SoroushAsadzadeh@Gmail.com>
2018-11-26 19:13:06 +0330

    Add the EntityListResult class to wrap pagination parameters. in case of working with client structure properly.

---
+++ Olive.GlobalSearch/Common/EntityListResult.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Olive.GlobalSearch
+{
+    /// <summary>
+    /// Represent a collection of data item include additional parameteres to store pagination arguments
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class EntityListResult<T>
+    {
+        /// <summary>
+        /// Represent the Collection of Data which is varied base on T element 
+        /// </summary>
+        public IEnumerable<T> Data;
+
+        /// <summary>
+        /// Represent the total count of items exsits 
+        /// </summary>
+        public long TotalCount { set; get; }
+
+        /// <summary>
+        /// Represent the start index (0 based) of the Data, which filled in Data property
+        /// </summary>
+        public int StartIndex { set; get; }
+
+        /// <summary>
+        /// Represent the Items per page 
+        /// </summary>
+        public int Size { set; get; }
+    }
+}
--- Olive.GlobalSearch/Common/SearchSource.cs
+++ Olive.GlobalSearch/Common/SearchSource.cs
@@ -12,7 +12,7 @@
     /// </summary>
     public abstract partial class SearchSource
     {
-        internal List<SearchResult> Results = new List<SearchResult>();
+        internal EntityListResult<SearchResult> Results = new EntityListResult<SearchResult>();
 
         /// <summary>
         /// A set of keywords specified by the user.
@@ -21,7 +21,7 @@
         /// </summary>
         public string[] Keywords { get; internal set; }
 
-        
+
 
         /// <summary>
         /// Determines whether the specified object's ToString() value or any of its specified properties together will match all the specified keywords of the search.
@@ -36,6 +36,8 @@
 
         /// <summary>
         /// Adds an item to the results.
+        /// If you Use this method to add items, Pagination arguments will be set automatically
+        /// Note: Do not use Set method if you use Add method.
         /// </summary>
         protected void Add(SearchResult result)
         {
@@ -49,7 +51,13 @@
 
             result.Url = FixUrl(result.Url);
             result.IconUrl = FixUrl(result.IconUrl);
-            Results.Add(result);
+
+            var list = Results.Data as List<SearchResult>;
+            if (list == null) list = new List<SearchResult>();
+            list.Add(result);
+            Results.Data = list;
+            Results.TotalCount++;
+            Results.Size = (int)Results.TotalCount;
         }
 
         static string FixUrl(string url)
@@ -60,11 +68,48 @@
 
         /// <summary>
         /// Adds an item to the results.
+        /// Note: Do not use Set method if you use Add method.
         /// <paramref name="url">For relative Url to the current site use ~/my-url syntax.</paramref>
         /// </summary>
         protected void Add(string title, string url)
         {
             Add(new SearchResult { Title = title, Url = url });
+        }
+
+        /// <summary>
+        /// Set Results, You can use this method to set data all in once
+        /// Note: Do not use Add method if you use Set method.
+        /// </summary>
+        /// <param name="result"></param>
+        protected void Set(EntityListResult<SearchResult> result)
+        {
+            if (result == null)
+                throw new ArgumentException("Null value can not set to result.");
+            if (result.Data == null)
+                throw new ArgumentException("Data Cannot be null, it should be an instance of IEnumerable object.");
+            if (result.TotalCount < 0)
+                throw new ArgumentException("TotalCount can not be lower that 0.");
+            if (result.Size < 0)
+                throw new ArgumentException("Size can not be lower that 0.");
+            if (result.StartIndex < 0)
+                throw new ArgumentException("StartIndex can not be lower that 0.");
+            if (result.StartIndex > result.TotalCount)
+                throw new ArgumentException("StartIndex can not be greater that TotalCount.");
+
+            Results = result;
+        }
+
+        /// <summary>
+        /// Set Results, You can use this method to set data all in once
+        /// Note: Do not use Add method if you use Set method.
+        /// </summary>
+        /// <param name="Data">A collection of SearchResult such as list,array or other IEnumerable of SearchResult.</param>
+        /// <param name="TotalCount">Total count of result</param>
+        /// <param name="StartIndex">Starting Index of Data, using in pagination arguments</param>
+        /// <param name="Size">The size of items per page, using in pagination arguments</param>
+        protected void Set(IEnumerable<SearchResult> Data, long TotalCount, int StartIndex, int Size)
+        {
+            Set(new EntityListResult<SearchResult>() { Data = Data, TotalCount = TotalCount, StartIndex = StartIndex, Size = Size });
         }
     }
 }
